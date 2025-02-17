3f345772255f18c223fcfc344b6fae46eb2bd68c
Saish Mungase <saishmungase@gmail.com>
2025-02-17 13:42:35 +0530

    Complete Learning Linked List Collections

---
+++ LinkedList/LLCollections.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LLCollections {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        System.out.println(ll);
+    }
+}
