c5bfc4f7dc3f00f4b06c044b711b085e15555fab
Saish Mungase <saishmungase@gmail.com>
2025-02-21 19:27:04 +0530

    Adding At Stack Bottom

---
+++ Stack/AddAtStackBottom.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class AddAtStackBottom {
+
+    public static void insertAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+
+        int popedData = s.pop();
+        insertAtBottom(s, data);
+        s.push(popedData);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        insertAtBottom(s, 4);
+        
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
