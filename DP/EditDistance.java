849b56f187d017128df6aeab8a50dfe6cd2aecc1
Saish Mungase <saishmungase@gmail.com>
2026-03-21 00:12:25 +0530

    DP : Solved Edit Distance using Tabulation

---
+++ DP/EditDistance.java
@@ -0,0 +1,35 @@
+public class EditDistance {
+
+    public static int ed(String w1, String w2){
+        int n = w1.length();
+        int m = w2.length();
+        int dp[][] = new int[n+1][m+1];
+
+        for(int i = 0; i< n; i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0; j < m; j++){
+            dp[0][j] = j;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(w1.charAt(i-1) == w2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+
+        System.out.println(ed(word1, word2));
+    }
+}
